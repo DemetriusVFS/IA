653da7459346390e3bee539ed3b75767e299fd7b
DemetriusVFS <demetriusvfsilva@fac.pe.senac.br>
2024-09-13 20:13:28 -0300

    maquina de estado finito

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿int transicao, estado;
+
+Console.WriteLine("Digite o Estado indicando de 1 a 3");
+estado = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite a Transição indicando 0 ou 1");
+transicao = int.Parse(Console.ReadLine());
+
+if (estado == 1 & transicao == 0 | estado == 2 & transicao == 0)
+{
+    Console.WriteLine("Seu estado atual é: 3");
+}
+else if (estado == 3 & transicao == 1)
+{
+    Console.WriteLine("Seu estado atual é: 2");
+}
+else if (estado == 2 & transicao == 1)
+{
+    Console.WriteLine("Seu estado atual é: 1");
+
+}
